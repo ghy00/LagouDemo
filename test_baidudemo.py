3a69108f444fea00f449b1d8a372959375207775
Lanterns <ghywyyx01@163.com>
2021-11-17 21:08:23 +0800

    代码优化

--- test_baidudemo.py
+++ test_baidudemo.py
@@ -13,14 +13,15 @@ def test_steps_demo(test_data1):
         driver = webdriver.Chrome()
         driver.get('https://www.baidu.com')
 
-    with allure.step("输入搜索内容并点击"):
+    with allure.step(f"输入{test_data1}并点击"):
         driver.find_element(By.ID, 'kw').send_keys(test_data1)
         time.sleep(2)
         driver.find_element(By.ID, "su").click()
         time.sleep(2)
 
-    with allure.step("保存测试截图并退出"):
+    with allure.step("保存测试截图"):
         driver.save_screenshot('./resource/b.png')
         allure.attach.file('./resource/b.png', attachment_type=allure.attachment_type.PNG)
         allure.attach('<head></head>首页<body></body>', '这是一个html', attachment_type=allure.attachment_type.HTML)
+    with allure.step("退出浏览器"):
         driver.quit()
